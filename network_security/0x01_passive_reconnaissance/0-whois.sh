aabccaca9c7e9247a04cb1b2d4e0fe403d1e3dfd
Anar-star <suvarovanar2@gmail.com>
2025-12-24 12:52:05 +0400

    Performed the 0th task of Passive Reconnaisance

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/{print $0}'
+whois "$1" | awk -F: '
+/^(Registrant|Admin|Tech)/ {
+    field=$1
+    value=$2
+    sub(/^ /,"",value)
+
+    if (field ~ /Street/) value=value " "
+    if (field ~ /Phone Ext|Fax Ext/) {
+        print field ":, "
+        next
+    }
+
+    print field ", " value
+}' > "$1.csv"
