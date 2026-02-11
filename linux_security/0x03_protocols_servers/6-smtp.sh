6d7ec7ef263acab55a5172736f52b1b6751938f6
Anar-star <suvarovanar2@gmail.com>
2026-02-11 19:51:28 +0000

    the 6th task Protocols and Servers

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -v '^#' /etc/postfix/main.cf | grep -q "smtpd_tls_security_level" && grep -v '^#' /etc/postfix/main.cf | grep "smtpd_tls_security_level" || echo "STARTTLS not configured"
+grep "^smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
