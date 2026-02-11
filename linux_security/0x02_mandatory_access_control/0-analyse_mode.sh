a2f63b8e56a092280db2ca7bf808222fb6697b94
Anar-star <suvarovanar2@gmail.com>
2026-02-11 16:42:01 +0000

    the 0th task of Mandatory Access Control

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if command -v getenforce >/dev/null 2>&1; then
-    mode=$(getenforce)
-    echo "SELinux status:                 $mode"
-else
-    echo "SELinux is not installed on this system."
-fi
-EOF
+getenforce | awk '{print "SELinux status:                 "$1}'
