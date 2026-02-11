34ba0fe588fd9d693cd6a37fa9707ee7eb8d944a
Anar-star <suvarovanar2@gmail.com>
2026-02-11 20:03:08 +0000

    the 9th task Protocols and Servers

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+#!/usr/bin/env bash
 iptables -P INPUT DROP
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
